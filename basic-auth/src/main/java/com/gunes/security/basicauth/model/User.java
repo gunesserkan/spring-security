28820fc3f73a5e35c6f7633bc4da04caeaf3c539
gunesserkan <gunesserkan883@gmail.com>
2024-06-14 01:29:41 +0300

    Added User model

---
+++ basic-auth/src/main/java/com/gunes/security/basicauth/model/User.java
@@ -0,0 +1,39 @@
+package com.gunes.security.basicauth.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+
+    private boolean accountNonExpired;
+    private boolean isEnable;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+
+    @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
+    @JoinTable(name = "authorities", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> authorities;
+
+    @Override
+    public boolean isEnabled() {
+        return isEnable;
+    }
+}
