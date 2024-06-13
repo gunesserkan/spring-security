e029d92ea0ca72e3bf79593dd8a075d344731259
gunesserkan <gunesserkan883@gmail.com>
2024-06-14 01:30:01 +0300

    Added Role model

---
+++ basic-auth/src/main/java/com/gunes/security/basicauth/model/Role.java
@@ -0,0 +1,14 @@
+package com.gunes.security.basicauth.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MOD;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
